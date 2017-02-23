761551e2571c101e38a4a7754063ff75ce0f45a7
Christine Emrich <christine.emrich@web.de>
2017-02-23 20:58:07 +0100

    Add channel subtitles

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 	var menu = jQuery('#channel-menu');
 	var playerNode = jQuery('#player');
 	var player = videojs('#player');
-	var menuItem = jQuery('<a class="channel"><img/></a>');
+	var menuItem = jQuery('<a class="channel"><img/><span class="subtitle"></span></a>');
 	var channels = {};
 
 	jQuery.getJSON('data/channels.json', function(json) {
@@ -54,9 +54,16 @@
 			var channel = channels[id];
 			var item = menuItem.clone();
 			var img = item.find('img');
+			var subtitle = item.find('.subtitle');
 			img.attr('alt', channel.name);
 			img.attr('title', channel.name);
 			img.attr('src', channel.logo_path);
+			if (channel.subtitle) {
+				subtitle.text(channel.subtitle);
+				subtitle.show();
+			} else {
+				subtitle.hide();
+			}
 			item.attr('href', '#' + channel.id);
 			menu.append(item);
 		}
